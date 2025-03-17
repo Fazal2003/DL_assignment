26bc93474ba0c2b8f1f19307a35369cc80efc48e
Mohammed Fazalullah <79568881+Fazal2003@users.noreply.github.com>
2025-03-17 15:12:18 +0530

    finla

--- assignment_!.py
+++ assignment_!.py
@@ -45,12 +45,41 @@ class loss:
 
     def cross_entropy(self,yh,y):
         l=y*np.log(yh+1e-5)
-        return -np.sum(l)
-
-class momuntum:
-  def 
+        return -np.sum(l)   
 
 
+
+def momentum(weights,biases,dw,db,vw,vb,learning_rate,b=0.9):
+    # vw=b*vw+(1-b)*dw
+    # vb=b*vb+(1-b)*db
+    # weights-=learning_rate*vw
+    # biases-=learning_rate*vb
+    # return weights,biases,vw,vb
+    ut=0
+    for i in range(steps):
+      ut+=(b**(steps-i)+dw)
+      weights-=learning_rate*ut
+      return weights
+
+
+
+
+
+def nestrov(weights,biases,db,dw,vw,vB,learning_rate,b=0.9):
+    # uw=weights-b*vW
+    # ub=biases-b*vB
+
+    # vW = b*vW+(1-b)*dw
+    # vB = b*vB+(1-b)*db
+    # weights-=learning_rate*vW
+    # biases-=learning_rate*vB
+    # return weights,biases,vW,vB
+
+    ut=0
+    for i in range(steps):
+      ut+=(b**(steps-i) +(weights-b*ut[i-1]))
+      weights-=learning_rate*ut
+      return weights
 
 from keras.datasets import fashion_mnist
 (train_images,train_labels),(test_images,test_labels)=fashion_mnist.load_data()
@@ -70,6 +99,8 @@ activ=activation()
 cost=loss()
 learning_rate=0.005
 images=(train_images-np.min(train_images))/(np.max(train_images)-np.min(train_images))
+
+#os 
 
 
 for i in range(size):
@@ -92,12 +123,12 @@ for i in range(size):
         dL_dz2=dL_da2*(z2>0)
 
         dL_da1=np.dot(dL_dz2,layer2.weights.T)
-        dL_dz1=dL_da1*(z2>0)
+        dL_dz1=dL_da1*(z1>0)
 
 
-        dw3=np.dot(a3,dL_dz3.T)
-        dw2=np.dot(a2,dL_dz2.T)
-        dw1=np.dot(a1,dL_dz1.T)
+        dw3=np.dot(a2.T,dL_dz3)
+        dw2=np.dot(a2.T,dL_dz2)
+        dw1=np.dot(image.T,dL_dz1)
 
         layer3.weights-=(learning_rate*dw3)
         layer2.weights-=(learning_rate*dw2)
@@ -106,6 +137,7 @@ for i in range(size):
         layer2.biases-=(learning_rate*np.sum(dL_dz2,axis=0,keepdims=True))
         layer1.biases-=(learning_rate*np.sum(dL_dz1,axis=0,keepdims=True))
 
+        #momentum(layer1.weigths,dw1,db1)
         print(l)
 
   
